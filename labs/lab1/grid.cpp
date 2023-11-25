80b44fab148c48d4eb65425602a194dab91e6e64
skyreaper925 <volodinmaxim1995@gmail.com>
2023-11-25 14:39:54 +0300

    Refactor and debug code in Grid classes and CMakeLists.txt
    The purpose of this commit was to refactor the code and fix certain issues in the Grid classes within the 'grid.cpp' file. Debug messages were added to crucial points in the code and some parts were commented out with the aim of better understanding the functionality and flow of the program.
    
    Changes were also made to 'CMakeLists.txt' to update the build system by including 'grid.cpp' and 'grids.cpp' for compilation. The project's structure was further enforced by specifying program source files, include directories, and adding new executable lines.

--- labs/lab1/grid.cpp
+++ labs/lab1/grid.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
 template<typename T>
 class Grid final
 {
@@ -32,13 +36,15 @@ public:
             *it = t;
     }
 
-    T operator()(size_type y_idx, size_type x_idx) const
-    {
-        return data[y_idx * x_size + x_idx];
-    }
+//    T operator()(size_type y_idx, size_type x_idx) const
+//    {
+//        cout << "const ()" << endl;
+//        return data[y_idx * x_size + x_idx];
+//    }
 
     T &operator()(size_type y_idx, size_type x_idx)
     {
+        cout << "no const ()" << endl;
         return data[y_idx * x_size + x_idx];
     }
 
@@ -64,28 +70,26 @@ public:
 
     Proxy operator[](size_type y_idx)
     {
+        cout << "no const []" << endl;
         return Proxy(data + y_idx * x_size, x_size);
     }
 
-    Proxy operator[](size_type y_idx) const
-    {
-        return Proxy(data + y_idx * x_size, x_size);
-    }
-
-
-private:
-    T *data;
-    size_type const y_size, x_size;
-
+//    Proxy operator[](size_type y_idx) const
+//    {
+//        return Proxy(data + y_idx * x_size, x_size);
+//    }
 
     Grid<T> &operator=(T const &t)
     {
         for (auto it = data, end = data + x_size * y_size; it != end; ++it)
             *it = t;
-
+        cout << "= operator" << endl;
         return *this;
     }
 
+private:
+    T *data;
+    size_type const y_size, x_size;
 };
 
 #include<cassert>
@@ -97,18 +101,24 @@ int main()
     assert(2 == g.get_x_size());
 
     using gsize_t = Grid<int>::size_type;
+    Grid<float> g1(3, 2, 0.0f);
+    cout << g1(1, 1) << endl;
+    g1(1, 1) = 2;
 
-    for (gsize_t y_idx = 0; y_idx != g.get_y_size(); ++y_idx)
-        for (gsize_t x_idx = 0; x_idx != g.get_x_size(); ++x_idx)
-            assert(0.0f == g[y_idx][x_idx]);
+    cout << g1[1][1] << endl;
+    g1[1][1] = 2;
 
-    for (gsize_t y_idx = 0; y_idx != g.get_y_size(); ++y_idx)
-        for (gsize_t x_idx = 0; x_idx != g.get_x_size(); ++x_idx)
-            g[y_idx][x_idx] = 1.0f;
-
-    for (gsize_t y_idx = 0; y_idx != g.get_y_size(); ++y_idx)
-        for (gsize_t x_idx = 0; x_idx != g.get_x_size(); ++x_idx)
-            assert(1.0f == g(y_idx, x_idx));
+//    for (gsize_t y_idx = 0; y_idx != g.get_y_size(); ++y_idx)
+//        for (gsize_t x_idx = 0; x_idx != g.get_x_size(); ++x_idx)
+//            assert(0.0f == g[y_idx][x_idx]);
+//
+//    for (gsize_t y_idx = 0; y_idx != g.get_y_size(); ++y_idx)
+//        for (gsize_t x_idx = 0; x_idx != g.get_x_size(); ++x_idx)
+//            g[y_idx][x_idx] = 1.0f;
+//
+//    for (gsize_t y_idx = 0; y_idx != g.get_y_size(); ++y_idx)
+//        for (gsize_t x_idx = 0; x_idx != g.get_x_size(); ++x_idx)
+//            assert(1.0f == g(y_idx, x_idx));
 
 //    Grid<float, 3> const g3(2, 3, 4, 1.0f);
 //    assert(1.0f == g3(1, 1, 1));
